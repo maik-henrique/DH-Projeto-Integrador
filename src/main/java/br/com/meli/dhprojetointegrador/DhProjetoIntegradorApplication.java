3212e1b525b7aaeb34f51fdeb0c45d4980750564
Maik Henrique <maik.pereira@mercadolivre.com>
2022-04-22 11:04:06 -0300

    chore: base project structure

---
+++ src/main/java/br/com/meli/dhprojetointegrador/DhProjetoIntegradorApplication.java
@@ -0,0 +1,13 @@
+package br.com.meli.dhprojetointegrador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DhProjetoIntegradorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DhProjetoIntegradorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/meli/dhprojetointegrador/DhProjetoIntegradorApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.meli.dhprojetointegrador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DhProjetoIntegradorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
