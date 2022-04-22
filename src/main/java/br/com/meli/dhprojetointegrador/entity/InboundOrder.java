52ae5912fd87a28f218293e70eaddcfc8a4efe95
MatheusGuerra2022 <100309963+MatheusGuerra2022@users.noreply.github.com>
2022-04-22 15:41:13 -0300

    [Feature/create-class] - add relations between entities

--- src/main/java/br/com/meli/dhprojetointegrador/entity/InboundOrder.java
+++ src/main/java/br/com/meli/dhprojetointegrador/entity/InboundOrder.java
@@ -4,39 +4,32 @@ package br.com.meli.dhprojetointegrador.entity;
 import lombok.*;
 
 import javax.persistence.*;
-import java.math.BigDecimal;
 import java.time.LocalDate;
-import java.util.Date;
 import java.util.List;
 
-@Entity
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @Setter
 @Builder
-public class InboundOrder {
 
+@Entity(name = "inboundOrder")
+public class InboundOrder {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer orderNumber;
     private LocalDate orderDate;
 
-
     @ManyToOne
     @JoinColumn(name="agent_id", nullable=false)
     private Agent agent;
 
-    // Section
     @ManyToOne
     @JoinColumn(name="section_id", nullable=false)
-    private Agent agents;
-
+    private Section section;
 
     @OneToMany(mappedBy="inboundOrder")
     private List<BatchStock> batchStockList;
-
-
 
 }
