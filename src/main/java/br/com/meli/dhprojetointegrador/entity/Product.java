a279d68094f2a6b1d82c0b1601f35dfc10cf5831
Pedro Dalpa <pedro380dalpa@gmail.com>
2022-04-22 17:19:03 -0300

    fix: alter import

--- src/main/java/br/com/meli/dhprojetointegrador/entity/Product.java
+++ src/main/java/br/com/meli/dhprojetointegrador/entity/Product.java
@@ -9,6 +9,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
