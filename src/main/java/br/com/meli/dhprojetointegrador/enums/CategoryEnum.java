d892094190fd5af6f027a5973d65b85a77c41173
Pedro Dalpa <pedro380dalpa@gmail.com>
2022-04-25 11:56:10 -0300

    fix add value in categoryEnum

--- src/main/java/br/com/meli/dhprojetointegrador/enums/CategoryEnum.java
+++ src/main/java/br/com/meli/dhprojetointegrador/enums/CategoryEnum.java
@@ -1,5 +1,18 @@
 package br.com.meli.dhprojetointegrador.enums;
 
 public enum CategoryEnum {
+  FF("Congelado"),
+  RF("Refrigerado"),
+  FS("Fresco");
 
+  private String value;
+
+  CategoryEnum(String valor) {
+    this.value = valor;
+  }
+
+  public String getValue() {
+    return value;
+
+  }
 }
