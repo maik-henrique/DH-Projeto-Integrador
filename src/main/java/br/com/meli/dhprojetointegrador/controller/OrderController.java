66cabdead7efcf788fc2e2b3b439fe8765a6a112
Micaela Alves <micaela.alves@mercadolivre.com>
2022-04-25 16:11:53 -0300

    feat:method to return all items from CartProduct table

--- src/main/java/br/com/meli/dhprojetointegrador/controller/OrderController.java
+++ src/main/java/br/com/meli/dhprojetointegrador/controller/OrderController.java
@@ -1,10 +1,12 @@
 package br.com.meli.dhprojetointegrador.controller;
 
 
-import br.com.meli.dhprojetointegrador.service.OrderService;
+import br.com.meli.dhprojetointegrador.entity.CartProduct;
+import br.com.meli.dhprojetointegrador.service.CartProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
 @RequestMapping(OrderController.baseUri)
@@ -12,14 +14,13 @@ public class OrderController {
 
     public static final String baseUri = "/api/v1/fresh-products/orders/";
 
+
     @Autowired
-    private OrderService orderService;
+    private CartProductService cartProductService;
 
-
-
-    @PostMapping("")
-    public PurchaseOrder<PurchaseOrderDTO> PurchaseOrderProductRegistration(@Valid @RequestBody PurchaseOrderDTO dto, UriComponentsBuilder uriBuilder){
-
-
+    @GetMapping("/")
+    public ResponseEntity<?> ShowProductsOrder(@RequestParam Long idOrder) {
+        return new ResponseEntity<>(cartProductService.getProductsByOrderId(idOrder), HttpStatus.OK);
     }
+
 }
