fde06ca2f745ed91e20c183d653765098ed69ed5
Bruno Mendes <bruno.bmendes@mercadolivre.com>
2022-04-26 18:33:20 -0300

    TEST: completed unit tests

--- src/main/java/br/com/meli/dhprojetointegrador/service/validator/ValidadeProduct.java
+++ src/main/java/br/com/meli/dhprojetointegrador/service/validator/ValidadeProduct.java
@@ -5,12 +5,17 @@ import br.com.meli.dhprojetointegrador.entity.Product;
 import br.com.meli.dhprojetointegrador.exception.NotEnoughProductsException;
 import br.com.meli.dhprojetointegrador.exception.ProductNotFoundException;
 import br.com.meli.dhprojetointegrador.repository.ProductRepository;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.List;
 
 @Service
+@AllArgsConstructor
+@NoArgsConstructor
 public class ValidadeProduct {
 
     @Autowired
@@ -22,18 +27,16 @@ public class ValidadeProduct {
      * Description: Valida se determinado produto possui estoque suficiente
      */
     public Product validateQuantity(Integer qtd, Long id) {
-        Product product;
         try {
-            product = productRepository.getById(id);
-        } catch (Exception e ) {
+            Product product = productRepository.getById(id);
+            List<BatchStock> batchList = product.getBatchStockList();
+            int totalStock = batchList.stream().mapToInt(BatchStock::getCurrentQuantity).sum();
+            if (totalStock < qtd) {
+                throw new NotEnoughProductsException("The product " + product.getName() + " doesn't have enough stock for your purchase");
+            }
+            return product;
+        } catch (EntityNotFoundException e ) {
             throw new ProductNotFoundException("This product isn't on the database");
         }
-
-        List<BatchStock> batchList = product.getBatchStockList();
-        int totalStock = batchList.stream().mapToInt(BatchStock::getCurrentQuantity).sum();
-        if (totalStock < qtd) {
-            throw new NotEnoughProductsException("The product " + product.getName() + " doesn't have enough stock for your purchase");
-        }
-        return product;
     }
 }
--- src/main/java/br/com/meli/dhprojetointegrador/service/validator/ValidateBuyer.java
+++ src/main/java/br/com/meli/dhprojetointegrador/service/validator/ValidateBuyer.java
@@ -3,10 +3,16 @@ package br.com.meli.dhprojetointegrador.service.validator;
 import br.com.meli.dhprojetointegrador.entity.Buyer;
 import br.com.meli.dhprojetointegrador.exception.BuyerNotFoundException;
 import br.com.meli.dhprojetointegrador.repository.BuyerRepository;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityNotFoundException;
+
 @Service
+@AllArgsConstructor
+@NoArgsConstructor
 public class ValidateBuyer {
 
     @Autowired
@@ -20,7 +26,7 @@ public class ValidateBuyer {
     public Buyer getBuyer(Long id) {
         try {
             return buyerRepository.getById(id);
-        } catch (Exception e) {
+        } catch (EntityNotFoundException e) {
             System.out.println(e);
             throw new BuyerNotFoundException("This buyer is not on the database");
         }
