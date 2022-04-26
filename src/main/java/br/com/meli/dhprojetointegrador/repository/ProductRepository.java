8b5eec79a3f9337c7753e6379dc73d40ce6b43ae
MatheusGuerra2022 <100309963+MatheusGuerra2022@users.noreply.github.com>
2022-04-26 17:09:00 -0300

    fix: Fix problem in the POM file

--- src/main/java/br/com/meli/dhprojetointegrador/repository/ProductRepository.java
+++ src/main/java/br/com/meli/dhprojetointegrador/repository/ProductRepository.java
@@ -1,6 +1,7 @@
 package br.com.meli.dhprojetointegrador.repository;
 
 import br.com.meli.dhprojetointegrador.entity.Product;
+import br.com.meli.dhprojetointegrador.enums.CategoryEnum;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import org.springframework.stereotype.Repository;
