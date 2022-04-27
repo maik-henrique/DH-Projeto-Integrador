197b898abc08089311e761a26b21e479c6aa4c20
Pedro Dalpa <pedro380dalpa@gmail.com>
2022-04-27 10:45:46 -0300

    Revert "create mock in inboundOrderService teste"
    This reverts commit 8f500aad05781259fe5e184aad600c29bf096122.

--- src/test/java/br/com/meli/dhprojetointegrador/unit/service/InboundOrderServiceTests.java
+++ src/test/java/br/com/meli/dhprojetointegrador/unit/service/InboundOrderServiceTests.java
@@ -1,28 +1,33 @@
 package br.com.meli.dhprojetointegrador.unit.service;
 
-import br.com.meli.dhprojetointegrador.entity.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import br.com.meli.dhprojetointegrador.entity.Agent;
+import br.com.meli.dhprojetointegrador.entity.BatchStock;
+import br.com.meli.dhprojetointegrador.entity.Category;
+import br.com.meli.dhprojetointegrador.entity.InboundOrder;
+import br.com.meli.dhprojetointegrador.entity.Section;
 import br.com.meli.dhprojetointegrador.enums.CategoryEnum;
-import br.com.meli.dhprojetointegrador.exception.BusinessValidatorException;
 import br.com.meli.dhprojetointegrador.repository.InboundOrderRepository;
 import br.com.meli.dhprojetointegrador.service.AgentService;
 import br.com.meli.dhprojetointegrador.service.InboundOrderService;
 import br.com.meli.dhprojetointegrador.service.ProductService;
 import br.com.meli.dhprojetointegrador.service.SectionService;
 import br.com.meli.dhprojetointegrador.service.validator.IInboundOrderValidator;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-
-import org.mockito.junit.jupiter.MockitoExtension;
-
-import java.util.List;
-import java.util.Optional;
-import java.util.Set;
-
-import static org.mockito.ArgumentMatchers.anyInt;
-import static org.mockito.ArgumentMatchers.anyLong;
-import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class InboundOrderServiceTests {
@@ -48,7 +53,8 @@ public class InboundOrderServiceTests {
     @Test
     public void update_shouldProperlyCallSaveWithUpdatedObject_whenAllNestedObjectsAreRetrievedProperly() {
         Set<BatchStock> batchStockList = Set.of();
-        Section section = Section.builder().id(1L).category(Category.builder().name(CategoryEnum.FRIOS).build()).build();
+        Section section = Section.builder().id(1L).category(Category.builder().name(CategoryEnum.FRIOS).build())
+                .build();
         Agent agent = Agent.builder().id(1L).build();
 
         when(sectionService.findSectionById(anyLong())).thenReturn(section);
