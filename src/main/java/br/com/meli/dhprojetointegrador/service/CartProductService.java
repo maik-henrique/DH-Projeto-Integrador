73e9acff0d682eddbb699d05bde84f8d7d2964d3
Micaela Alves <micaela.alves@mercadolivre.com>
2022-04-28 16:12:17 -0300

    feat: add test - invalid id/ refactoring exception

--- src/main/java/br/com/meli/dhprojetointegrador/service/CartProductService.java
+++ src/main/java/br/com/meli/dhprojetointegrador/service/CartProductService.java
@@ -1,6 +1,7 @@
 package br.com.meli.dhprojetointegrador.service;
 
 import br.com.meli.dhprojetointegrador.entity.CartProduct;
+import br.com.meli.dhprojetointegrador.exception.BadRequestException;
 import br.com.meli.dhprojetointegrador.repository.CartProductRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -21,7 +22,11 @@ public class CartProductService{
      *
      **/
     public List<CartProduct> getProductsByOrderId(long OrderId) {
-        return cartProdRepo.findByPurchaseOrderId(OrderId);
+        List<CartProduct> list = cartProdRepo.findByPurchaseOrderId(OrderId);
+        if (list.isEmpty()) {
+            throw new BadRequestException("Order Id is not found");
+        }
+        return list;
     }
 
 }
