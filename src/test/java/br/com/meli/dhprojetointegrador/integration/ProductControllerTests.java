2bcbd7f853ecc247a9778352174be3cd76039ff2
MatheusGuerra2022 <100309963+MatheusGuerra2022@users.noreply.github.com>
2022-04-28 16:18:05 -0300

    feat/ Improve code quality for the tests

--- src/test/java/br/com/meli/dhprojetointegrador/integration/ProductControllerTests.java
+++ src/test/java/br/com/meli/dhprojetointegrador/integration/ProductControllerTests.java
@@ -21,6 +21,10 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
@@ -41,10 +45,20 @@ public class ProductControllerTests {
 
     @Autowired
     private CategoryRepository categoryRepository;
-    
 
+    @Autowired
+    private ObjectMapper objectMapper;
+
+
+    /**
+     * @Author: Matheus Guerra
+     * @Teste: Teste de integração do endpoint "/api/v1/fresh-products/list"
+     * @Description: valida o funcionamento correto do endpoint:
+     *              Retorno de uma lista de produtos filtrados por categoria;
+     *              Retorno de uma lista vazia caso não haja produtos da categoria escolhida;
+     */
     @Test
-    @DisplayName("US:02 - Item 02 - Testa se ta funcionando")
+    @DisplayName("US:02 - Item 02")
     public void correct_functioning_of_returnAllProductsByCategory() throws Exception {
 
         Category categoryFF = setupCategory(CategoryEnum.FF);
@@ -55,7 +69,14 @@ public class ProductControllerTests {
 
         Product productFSa = setupProduct("pera", categoryFS);
 
-        Product productWithoutCategory = setupProduct("objeto", null);
+        setupProduct("objeto", null);
+
+        List<Product> produtosFF = Arrays.asList(productFFa,productFFb);
+        List<Product> produtosFS = Arrays.asList(productFSa);
+
+        String payloadFF = objectMapper.writeValueAsString(produtosFF);
+        String payloadFS = objectMapper.writeValueAsString(produtosFS);
+
 
         MvcResult resultFF = mock
                 .perform(MockMvcRequestBuilders.get("/api/v1/fresh-products/list").param("category","FF"))
@@ -72,30 +93,23 @@ public class ProductControllerTests {
         String responsePayloadFF = resultFF.getResponse().getContentAsString();
         String responsePayloadFS = resultFS.getResponse().getContentAsString();
         String responsePayloadRF = resultRF.getResponse().getContentAsString();
-        System.out.println(responsePayloadRF);
-
 
         assertNotNull(responsePayloadFF);
         assertNotNull(responsePayloadFS);
         assertEquals("",responsePayloadRF);
 
-
-
-
-    }
-
-    private void setup() {
+        assertEquals(payloadFF, responsePayloadFF);
+        assertEquals(payloadFS, responsePayloadFS);
 
     }
-
 
     private Category setupCategory(CategoryEnum categoryEnum) {
-        Category category = Category.builder().name(categoryEnum).build();
+        Category category = Category.builder().name(categoryEnum).sections(Collections.EMPTY_SET).build();
         return categoryRepository.save(category);
     }
 
     private Product setupProduct(String name, Category category) {
-        Product product = Product.builder().name(name).category(category).build();
+        Product product = Product.builder().name(name).category(category).batchStockList(Collections.EMPTY_SET).build();
 
         productRepository.save(product);
         return product;
--- src/test/java/br/com/meli/dhprojetointegrador/unit/service/ProductServiceTests.java
+++ src/test/java/br/com/meli/dhprojetointegrador/unit/service/ProductServiceTests.java
@@ -3,28 +3,20 @@ package br.com.meli.dhprojetointegrador.unit.service;
 import br.com.meli.dhprojetointegrador.entity.Category;
 import br.com.meli.dhprojetointegrador.entity.Product;
 import br.com.meli.dhprojetointegrador.enums.CategoryEnum;
-import br.com.meli.dhprojetointegrador.repository.BuyerRepository;
-import br.com.meli.dhprojetointegrador.repository.CategoryRepository;
-import br.com.meli.dhprojetointegrador.repository.InboundOrderRepository;
 import br.com.meli.dhprojetointegrador.repository.ProductRepository;
-import br.com.meli.dhprojetointegrador.service.InboundOrderService;
 import br.com.meli.dhprojetointegrador.service.ProductService;
 import org.hamcrest.collection.IsEmptyCollection;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Arrays;
 import java.util.List;
 
-
-import static org.hamcrest.CoreMatchers.hasItems;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsInAnyOrder;
-import static org.hamcrest.Matchers.hasProperty;
 import static org.mockito.Mockito.*;
 
 
@@ -34,11 +26,7 @@ public class ProductServiceTests {
     @InjectMocks
     private ProductService productService;
 
-
     private ProductRepository productRepository = mock(ProductRepository.class);;
-
-    @Mock
-    private CategoryRepository categoryRepository;
 
     Category categoryFF = Category.builder()
             .id(1L)
@@ -57,11 +45,6 @@ public class ProductServiceTests {
             .category(categoryFF)
             .build();
 
-    Product productWithoutCategory = Product.builder()
-            .id(1L)
-            .name("queijo")
-            .build();
-
     List<Product> products = Arrays.asList(productFFa, productFFb);
 
     /**
@@ -75,9 +58,6 @@ public class ProductServiceTests {
 
         when(productRepository.findByCategory_Name(CategoryEnum.FF)).thenReturn(products);
         List<Product> result = productService.returnProductsByCategory("FF");
-
-        // Uma possibilidade
-        //assertThat(result, hasItems(productFFa, productFFb));
 
         assertThat(result, containsInAnyOrder(productFFa, productFFb));
     }
@@ -96,7 +76,5 @@ public class ProductServiceTests {
 
         assertThat(result, IsEmptyCollection.empty());
     }
-
-
 
 }
