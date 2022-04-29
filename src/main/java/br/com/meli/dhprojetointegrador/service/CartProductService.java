f390056c3aa4f5ee9c54a915a9a66850720588bd
Micaela Alves <micaela.alves@mercadolivre.com>
2022-04-28 22:56:13 -0300

    refactor - http status correspondente

--- src/main/java/br/com/meli/dhprojetointegrador/service/CartProductService.java
+++ src/main/java/br/com/meli/dhprojetointegrador/service/CartProductService.java
@@ -1,11 +1,10 @@
 package br.com.meli.dhprojetointegrador.service;
 
 import br.com.meli.dhprojetointegrador.entity.CartProduct;
-import br.com.meli.dhprojetointegrador.exception.BadRequestException;
+import br.com.meli.dhprojetointegrador.exception.ResourceNotFound;
 import br.com.meli.dhprojetointegrador.repository.CartProductRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -24,7 +23,7 @@ public class CartProductService{
     public List<CartProduct> getProductsByOrderId(long OrderId) {
         List<CartProduct> list = cartProdRepo.findByPurchaseOrderId(OrderId);
         if (list.isEmpty()) {
-            throw new BadRequestException("Order Id is not found");
+            throw new ResourceNotFound("Order Id is not found");
         }
         return list;
     }
