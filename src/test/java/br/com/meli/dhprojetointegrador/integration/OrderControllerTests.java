d47f4d147fb5d0a7fac89c99f2c5d8501f69cc6c
Micaela Alves <micaela.alves@mercadolivre.com>
2022-04-28 23:00:45 -0300

    feat: testes de integracao

--- src/test/java/br/com/meli/dhprojetointegrador/integration/OrderControllerTests.java
+++ src/test/java/br/com/meli/dhprojetointegrador/integration/OrderControllerTests.java
@@ -7,22 +7,12 @@ import br.com.meli.dhprojetointegrador.entity.*;
 import br.com.meli.dhprojetointegrador.enums.CategoryEnum;
 import br.com.meli.dhprojetointegrador.enums.StatusEnum;
 import br.com.meli.dhprojetointegrador.repository.*;
-import br.com.meli.dhprojetointegrador.service.CartProductService;
-import br.com.meli.dhprojetointegrador.unit.util.CardProductCreator;
-import br.com.meli.dhprojetointegrador.unit.util.ProductCreator;
-import br.com.meli.dhprojetointegrador.unit.util.PurchaseOrderCreator;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.test.annotation.DirtiesContext;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -33,11 +23,8 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Set;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.mock;
 
 public class OrderControllerTests extends BaseIntegrationControllerTests {
 
@@ -70,6 +57,9 @@ public class OrderControllerTests extends BaseIntegrationControllerTests {
 
     @Autowired
     private CartProductRepository cartProductRepository;
+
+    @Autowired
+    private PurchaseOrderRepository purchaseOrderRepository;
 
     @Test
     @DisplayName("Create Purchase Order - when receiving the right input function works properly")
@@ -187,51 +177,54 @@ public class OrderControllerTests extends BaseIntegrationControllerTests {
         assertEquals(exceptionResponse.getTitle(), "Not Enough Products");
     }
 
+    /**
+     * Author: Micaela Alves
+     * Test: Teste de integração do endpoint "/api/v1/fresh-products/orders"
+     * Description: verifica se o endpoint retorna a lista de produtos de uma determinada order
+     * @throws Exception
+     */
     @Test
     @DisplayName("Show Products in Purchase Order - when receiving valid id return all products")
     public void should_return_all_products_in_purchase_order() throws Exception{
-        /**
-         * BUYER ----------
-         * PURCHASE ORDER ---------------
-         * INBOUNDORDER ----------------
-         * AGENTE -------
-         * SELLER
-         */
-
-        setup();
         Product product1 = setupProduct(1L, "Banana", new BigDecimal("2.50"));
-
-       // CartProduct cartProduct = setupCartProduct(product1, );
-//        List<CartProduct> cardProductList = IntStream
-//                .range(0, 2)
-//                .mapToObj(value -> {
-//                    return CardProductCreator.createValidCardProduct();
-//                })
-//                .collect(Collectors.toList());
-
-        cartProductRepository.save(CardProductCreator.createValidCardProduct());
+        Buyer buyer = setupBuyer();
+        PurchaseOrder purchaseOrder = setupPurchaseOrder(buyer);
+        purchaseOrderRepository.save(purchaseOrder);
+        CartProduct cartProduct = setupCartProduct(product1, purchaseOrder, LocalDate.of(2020, 4, 22));
+        cartProductRepository.save(cartProduct);
 
         MvcResult result = mock
-                .perform(MockMvcRequestBuilders.get("/api/v1/fresh-products/orders/?idOrderd=1"))
-                .andExpect(MockMvcResultMatchers.status().isOk()).andReturn();
+                .perform(MockMvcRequestBuilders.get("/api/v1/fresh-products/orders/")
+                .param("idOrder", "1"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andReturn();
 
         String response = result.getResponse().getContentAsString();
-        List<CartProductDTO> products =
-                objectMapper.readerForListOf(CartProductDTO.class).readValue(response);
 
+        List<CartProductDTO> products = objectMapper.readerForListOf(CartProductDTO.class).readValue(response);
         assertEquals(HttpStatus.OK.value(), result.getResponse().getStatus());
         assertNotNull(response);
         assertFalse(products.isEmpty());
 
-//        CartProductRepository repository = mock(CartProductRepository.class);
-//        private final CartProductService service = new CartProductService(repository);
+    }
 
-//        Mockito.when(repository.findByPurchaseOrderId(id)).thenReturn(cardProductList);
-//        List<CartProduct> result = service.getProductsByOrderId(id);
-//
-//        assert result.get(0).getQuantity().equals(5);
-//        assert result.get(0).getProduct().getName().equals("Frango");
-//        assert result.get(1).getPurchaseOrder().getBuyer().getName().equals("Bruno");
+    /**
+     * Author: Micaela Alves
+     * Test: Teste de integração do endpoint "/api/v1/fresh-products/orders"
+     * Description: verifica se o endpoint NotFound quando o id informado for invalido
+     * @throws Exception
+     */
+    @Test
+    @DisplayName("Return 404 not found - when receiving invalid order id")
+    public void should_return_not_found_when_invalid_orderId() throws Exception{
+        MvcResult result = mock
+                .perform(MockMvcRequestBuilders.get("/api/v1/fresh-products/orders/")
+                        .param("idOrder", "1"))
+                .andExpect(MockMvcResultMatchers.status().isNotFound())
+                .andReturn();
+
+        assertEquals(HttpStatus.NOT_FOUND.value(), result.getResponse().getStatus());
+
     }
 
     private void setup() {
@@ -247,7 +240,6 @@ public class OrderControllerTests extends BaseIntegrationControllerTests {
 
         setupInboundOrder(1L, batchStock1, warehouse, section);
         setupInboundOrder(2L, batchStock2, warehouse, section);
-
     }
 
     private Buyer setupBuyer() {
@@ -315,38 +307,22 @@ public class OrderControllerTests extends BaseIntegrationControllerTests {
         return inboundOrderRepository.save(inboundOrder);
     }
 
-    private CartProduct setupCartProduct(Product prod, PurchaseOrder order, int quantity) {
+    private CartProduct setupCartProduct(Product prod, PurchaseOrder order, LocalDate of) {
         return CartProduct.builder()
                 .product(prod)
                 .purchaseOrder(order)
-                .quantity(quantity)
+                .quantity(5)
                 .build();
     }
 
-    private PurchaseOrder setupPurchaseOrder(Buyer buyer, StatusEnum status, LocalDate date){
+    private PurchaseOrder setupPurchaseOrder(Buyer buyer){
         return PurchaseOrder.builder()
+                .id(1L)
                 .buyer(buyer)
-                .status(status)
-                .date(date)
+                .status(StatusEnum.ABERTO)
+                .date(LocalDate.of(2020, 4, 22))
                 .build();
     }
 
-    private Agent setupAgent(Warehouse warehouse) {
-        return Agent.builder()
-                .id(1L)
-                .name("Eduardo")
-                .password("12345")
-                .warehouse(warehouse)
-                .build();
 
-    }
-    private Seller setupSeller(Set<Product> products) {
-        return Seller.builder()
-                .id(1L)
-                .name("Mauricio")
-                .products(products)
-                .build();
-
-
-    }
 }
