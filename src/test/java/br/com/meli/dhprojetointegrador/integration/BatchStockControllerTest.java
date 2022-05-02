ba87919296f026d981ace405ca658d12a02b441c
Pedro Dalpa <pedro380dalpa@gmail.com>
2022-05-02 17:11:43 -0300

    finish batch stock integration tests

--- src/test/java/br/com/meli/dhprojetointegrador/integration/BatchStockControllerTest.java
+++ src/test/java/br/com/meli/dhprojetointegrador/integration/BatchStockControllerTest.java
@@ -14,9 +14,11 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
@@ -24,7 +26,6 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import br.com.meli.dhprojetointegrador.dto.BatchStockDTO;
-import br.com.meli.dhprojetointegrador.dto.request.BatchStockUpdateRequest;
 import br.com.meli.dhprojetointegrador.entity.Agent;
 import br.com.meli.dhprojetointegrador.entity.BatchStock;
 import br.com.meli.dhprojetointegrador.entity.Category;
@@ -33,7 +34,6 @@ import br.com.meli.dhprojetointegrador.entity.Product;
 import br.com.meli.dhprojetointegrador.entity.Section;
 import br.com.meli.dhprojetointegrador.entity.Warehouse;
 import br.com.meli.dhprojetointegrador.enums.CategoryEnum;
-import br.com.meli.dhprojetointegrador.repository.AgentRepository;
 import br.com.meli.dhprojetointegrador.repository.BatchStockRepository;
 import br.com.meli.dhprojetointegrador.repository.CategoryRepository;
 import br.com.meli.dhprojetointegrador.repository.InboundOrderRepository;
@@ -44,15 +44,14 @@ import br.com.meli.dhprojetointegrador.repository.WarehouseRepository;
 @ActiveProfiles("test")
 @SpringBootTest
 @AutoConfigureMockMvc
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.ANY)
 public class BatchStockControllerTest {
     @Autowired
     private MockMvc mock;
 
     @Autowired
     private SectionRepository sectionRepository;
-
-    @Autowired
-    private AgentRepository agentRepository;
 
     @Autowired
     private WarehouseRepository warehouseRepository;
@@ -75,21 +74,11 @@ public class BatchStockControllerTest {
     @Test
     @DisplayName("Retornar os BatchStocks de acordo com os parametros")
     public void shouldReturnAllBatchStockFindByParam() throws Exception {
-        setupBaseData(42.0f);
-
-        BatchStockUpdateRequest expectedBatchStock = BatchStockUpdateRequest.builder()
-                .batchNumber(123L)
-                .currentQuantity(4)
-                .initialQuantity(4)
-                .currentTemperature(24.0f)
-                .minimumTemperature(19f)
-                .manufacturingDate(LocalDate.of(2022, 5, 23))
-                .manufacturingTime(LocalDateTime.of(2016, 12, 30, 14, 23, 25))
-                .dueDate(LocalDate.of(2022, 4, 22))
-                .productId(2L).build();
+        setupBaseData();
 
         MvcResult result = mock
-                .perform(MockMvcRequestBuilders.get("/api/v1/fresh-products/due-date").param("sectionId", "1"))
+                .perform(MockMvcRequestBuilders.get("/api/v1/fresh-products/due-date")
+                        .param("sectionId", "1"))
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andReturn();
 
@@ -99,50 +88,82 @@ public class BatchStockControllerTest {
         assertEquals(HttpStatus.OK.value(), result.getResponse().getStatus());
         assertNotNull(responsePayload);
         assertFalse(batchStocks.isEmpty());
-
-        System.out.println(batchStocks + "   AAAAAA \n\n\n\n");
+        assertEquals(batchStocks.size(), 1);
 
     }
+
     @Test
-    @DisplayName("Retornar batchstock pela categoria")
-    public void shouldReturnBatchstockFindByCategory() throws Exception {
-        setupBaseData(42.0f);
-
-
-        BatchStockUpdateRequest expectedBatchStock = BatchStockUpdateRequest.builder()
-                .batchNumber(123L)
-                .currentQuantity(4)
-                .initialQuantity(4)
-                .currentTemperature(24.0f)
-                .minimumTemperature(19f)
-                .manufacturingDate(LocalDate.of(2022, 5, 23))
-                .manufacturingTime(LocalDateTime.of(2016, 12, 30, 14, 23, 25))
-                .dueDate(LocalDate.of(2022, 4, 22))
-                .productId(2L).build();
+    @DisplayName("Retornar batchStock pela categoria")
+    public void shouldReturnBatchStockFindByCategory() throws Exception {
+        setupBaseData();
 
         MvcResult result = mock
-                .perform(MockMvcRequestBuilders.get("/api/v1/fresh-products/due-date/").param("category", "FF").param("sectionId", "1"))
+                .perform(MockMvcRequestBuilders.get("/api/v1/fresh-products/due-date/")
+                        .param("category", "FF")
+                        .param("sectionId", "1"))
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andReturn();
 
         String responsePayload = result.getResponse().getContentAsString();
         List<BatchStockDTO> batchStocks = objectMapper.readerForListOf(BatchStockDTO.class).readValue(responsePayload);
 
-        System.out.println(batchStocks.get(0).getCategory_id());
-
         assertEquals(HttpStatus.OK.value(), result.getResponse().getStatus());
         assertNotNull(responsePayload);
-
+        assertFalse(batchStocks.isEmpty());
+        assertEquals(batchStocks.size(), 1);
 
     }
 
-    private void setupBaseData(float newPoductVolume) {
+    @Test
+    @DisplayName("Retornar batchStock pela data de validade")
+    public void shouldReturnBatchStockFindByDueDate() throws Exception {
+        setupBaseData();
+
+        MvcResult result = mock
+                .perform(MockMvcRequestBuilders.get("/api/v1/fresh-products/due-date/")
+                        .param("numberOfDays", "20")
+                        .param("sectionId", "2"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andReturn();
+
+        String responsePayload = result.getResponse().getContentAsString();
+        List<BatchStockDTO> batchStocks = objectMapper.readerForListOf(BatchStockDTO.class).readValue(responsePayload);
+
+        assertEquals(HttpStatus.OK.value(), result.getResponse().getStatus());
+        assertNotNull(responsePayload);
+        assertFalse(batchStocks.isEmpty());
+        assertEquals(batchStocks.size(), 1);
+
+    }
+
+    @Test
+    @DisplayName("Retornar batchStock aplicando todos os filtros")
+    public void shouldReturnBatchStockApplierAllFilters() throws Exception {
+        setupBaseData();
+
+        MvcResult result = mock
+                .perform(MockMvcRequestBuilders.get("/api/v1/fresh-products/due-date/")
+                        .param("numberOfDays", "20")
+                        .param("sectionId", "3")
+                        .param("category", "FF"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andReturn();
+
+        String responsePayload = result.getResponse().getContentAsString();
+        List<BatchStockDTO> batchStocks = objectMapper.readerForListOf(BatchStockDTO.class).readValue(responsePayload);
+
+        assertEquals(HttpStatus.OK.value(), result.getResponse().getStatus());
+        assertNotNull(responsePayload);
+        assertFalse(batchStocks.isEmpty());
+        assertEquals(batchStocks.size(), 1);
+
+    }
+
+    private void setupBaseData() {
         Category managedCategory = setupCategory(CategoryEnum.FF);
         Warehouse managedWarehouse = setupWarehouse();
         Section managedSection = setupSection(managedCategory, managedWarehouse);
         Product managedProduct = setupProduct(managedCategory, 2.0f);
-
-        setupProduct(managedCategory, newPoductVolume);
 
         InboundOrder manegeInboundOrder = setupInboundOrder(managedWarehouse, managedSection);
         setupBatchStock(managedProduct, manegeInboundOrder);
@@ -164,21 +185,22 @@ public class BatchStockControllerTest {
 
     private Section setupSection(Category managedCategory, Warehouse managedWarehouse) {
         Section section = Section.builder().category(managedCategory).name("Cold section").capacity(32.0f)
-                .warehouse(managedWarehouse).id(1L).build();
+                .warehouse(managedWarehouse).build();
 
         return sectionRepository.save(section);
     }
 
     private Warehouse setupWarehouse() {
         Agent agent = Agent.builder().name("007").build();
-        Warehouse warehouse = Warehouse.builder().id(1L).name("Galpao do joao").agent(agent).build();
+        Warehouse warehouse = Warehouse.builder().name("Galpao do joao").agent(agent).build();
         agent.setWarehouse(warehouse);
 
         return warehouseRepository.save(warehouse);
     }
 
-    private BatchStock setupBatchStock(Product managedProduct, InboundOrder inboundOrder) {
-        LocalDate TODAY = LocalDate.now().plusDays(15);
+    private BatchStock setupBatchStock(Product managedProduct, InboundOrder manegeInboundOrder) {
+        LocalDate TODAY_MORE_FIFTEEN = LocalDate.now().plusDays(15);
+        LocalDate TODAY_MORE_THIRTEEN = LocalDate.now().plusDays(30);
         BatchStock batchStock = BatchStock.builder()
                 .batchNumber(123L)
                 .currentQuantity(3)
@@ -186,8 +208,46 @@ public class BatchStockControllerTest {
                 .currentTemperature(24f)
                 .manufacturingDate(LocalDate.of(2020, 4, 22))
                 .manufacturingTime(LocalDateTime.of(2016, 10, 30, 14, 23, 25))
-                .dueDate(TODAY)
+                .dueDate(TODAY_MORE_FIFTEEN)
+                .inboundOrder(manegeInboundOrder)
+                .products(managedProduct)
+                .build();
+
+        batchStockRepository.save(batchStock);
+
+        Category category = setupCategory(CategoryEnum.FS);
+        Warehouse warehouse = setupWarehouse();
+        Section section = setupSection(category, warehouse);
+        Product product = setupProduct(category, 2.0f);
+        InboundOrder inboundOrder = setupInboundOrder(warehouse, section);
+
+        Category otherCategory = setupCategory(CategoryEnum.FF);
+        Section otherSection = setupSection(otherCategory, warehouse);
+        InboundOrder otherInboundOrder = setupInboundOrder(warehouse, otherSection);
+
+        batchStock = BatchStock.builder()
+                .batchNumber(124L)
+                .currentQuantity(3)
+                .initialQuantity(4)
+                .currentTemperature(24f)
+                .manufacturingDate(LocalDate.of(2020, 4, 22))
+                .manufacturingTime(LocalDateTime.of(2016, 10, 30, 14, 23, 25))
+                .dueDate(TODAY_MORE_THIRTEEN)
                 .inboundOrder(inboundOrder)
+                .products(product)
+                .build();
+
+        batchStockRepository.save(batchStock);
+
+        batchStock = BatchStock.builder()
+                .batchNumber(125L)
+                .currentQuantity(3)
+                .initialQuantity(4)
+                .currentTemperature(24f)
+                .manufacturingDate(LocalDate.of(2020, 4, 22))
+                .manufacturingTime(LocalDateTime.of(2016, 10, 30, 14, 23, 25))
+                .dueDate(TODAY_MORE_THIRTEEN)
+                .inboundOrder(otherInboundOrder)
                 .products(managedProduct)
                 .build();
 
@@ -198,7 +258,6 @@ public class BatchStockControllerTest {
             Section managedSection) {
 
         InboundOrder inboundOrder = InboundOrder.builder()
-                .orderNumber(0L)
                 .section(managedSection)
                 .orderDate(LocalDate.of(2020, 3, 4))
                 .agent(managedWarehouse.getAgent())
