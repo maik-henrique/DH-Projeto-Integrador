2e0d12d2da7524b83fecf4bad90ea15fd91753af
David Alexandre Fernandes <davidalexandrefernandes@Outlook.com>
2022-05-04 12:04:20 -0300

    config: Redis

--- src/main/java/br/com/meli/dhprojetointegrador/config/SwaggerConfig.java
+++ src/main/java/br/com/meli/dhprojetointegrador/config/SwaggerConfig.java
@@ -10,6 +10,7 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@Configuration
 @EnableSwagger2
 public class SwaggerConfig {
 
@@ -17,7 +18,7 @@ public class SwaggerConfig {
         public Docket api() {
             return new Docket(DocumentationType.SWAGGER_2)
                     .select()
-                    .apis(RequestHandlerSelectors.any())
+                    .apis(RequestHandlerSelectors.basePackage("br.com.meli.dhprojetointegrador.controller"))
                     .paths(PathSelectors.any())
                     .build()
                     .apiInfo(apiInfo());
@@ -25,45 +26,13 @@ public class SwaggerConfig {
 
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
+                .license("Common Licenses")
                 .title("Projeto Integrador")
                 .description("Bootcamp - Mercado Livre")
                 .version("1.0.0")
-                .license("Apache License Version 2.0")
-                .licenseUrl("https://www.apache.org/licenses/LICENSE-2.0")
-                        .contact(new Contact("JackPot Squad", "https://davidalexandrefernandes.atlassian.net/l/c/Bed22y32", "mercadolivre@mercadolivre.com"))
-                        .build();
-    }
-
-   /* private SecurityContext securityContext() {
-        return SecurityContext.builder()
-                .securityReferences(defaultAuth())
-                .forPaths(PathSelectors.ant("/pessoa/**"))
+                .contact(new Contact("JackPot Squad", "https://davidalexandrefernandes.atlassian.net/l/c/Bed22y32", "mercadolivre@mercadolivre.com"))
                 .build();
     }
 
-    List<SecurityReference> defaultAuth() {
-        AuthorizationScope authorizationScope
-                = new AuthorizationScope("ADMIN", "accessEverything");
-        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
-        authorizationScopes[0] = authorizationScope;
-        return Arrays.asList(
-                new SecurityReference("Token Access", authorizationScopes));
-    }*/
-
-
-   /* private List<ResponseMessage> responseMessage()
-    {
-        return new ArrayList<ResponseMessage>() {{
-            add(new ResponseMessageBuilder()
-                    .code(500)
-                    .message("500 message")
-                    .responseModel(new ModelRef("Error"))
-                    .build());
-            add(new ResponseMessageBuilder()
-                    .code(403)
-                    .message("Forbidden!")
-                    .build());
-        }};
-    }*/
 
 }
