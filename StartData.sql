1568455eb6c6646b7c49c7dea8ea48e66c42d56b
Pedro Dalpa <pedro380dalpa@gmail.com>
2022-05-06 08:55:21 -0300

    fix sql file

--- StartData.sql
+++ StartData.sql
@@ -9,12 +9,12 @@ VALUES(1, "Pedro", 1);
 INSERT INTO `meli-frescos`.category
 (id, maximum_temperature, minimum_temperature, name)
 VALUES(1, 5, -10, "FF"),
-VALUES(1, 15, 0, "FS"),;
+(1, 15, 0, "FS");
 
 INSERT INTO `meli-frescos`.`section`
 (id, capacity, name, category_id, warehouse)
 VALUES(1, 10, "Section 1", 1, 1),
-VALUES(2, 10, "Section 1", 2, 1);
+(2, 10, "Section 1", 2, 1);
 
 INSERT INTO `meli-frescos`.seller
 (id, name)
@@ -23,7 +23,7 @@ VALUES(1, "Seller 1");
 INSERT INTO `meli-frescos`.product
 (id, name, price, volume, category_id, seller_id)
 VALUES(1, "Frango", 20, 2, 1, 1),
-VALUES(1, "Alface", 10, 2, 2, 1);
+(1, "Alface", 10, 2, 2, 1);
 
 INSERT INTO `meli-frescos`.buyer
 (email, name, status, fk_user_id)
@@ -31,22 +31,22 @@ VALUES("buyer@gmail.com", "buyer", "ATIVADO", 3);
 
 INSERT INTO `meli-frescos`.purchase_order
 (`date`, status, buyer_id)
-VALUES("2022-05-14", "ABERTO", 1)
-VALUES("2022-05-14", "FINALIZADO", 1);
+VALUES("2022-05-14", "ABERTO", 1), 
+("2022-05-14", "FINALIZADO", 1);
 
 INSERT INTO `meli-frescos`.cart_product
 (quantity, product_id, purchase_order_id)
-VALUES(2, 1, 1)
-VALUES(3, 2, 2);
+VALUES(2, 1, 1),
+(3, 2, 2);
 
 INSERT INTO `meli-frescos`.inbound_order
 (order_date, agent_id, section_id)
-VALUES("2022-05-01", 1, 1)
-VALUES("2022-05-01", 1, 2);
+VALUES("2022-05-01", 1, 1),
+("2022-05-01", 1, 2);
 
 INSERT INTO `meli-frescos`.batch_stock
 (batch_number, current_quantity, current_temperature, due_date, initial_quantity, manufacturing_date, manufacturing_time, minimum_temperature, order_number, product_id)
-VALUES(1, 10, 0, "2022-06-20", 15, "2022-04-10", '2022-04-10 10:00', -10, 1, 1)
-VALUES(2, 10, 20, "2022-06-20", 15, "2022-04-10", '2022-04-10 10:00', 18, 2, 2);
+VALUES(1, 10, 0, "2022-06-20", 15, "2022-04-10", '2022-04-10 10:00', -10, 1, 1),
+(2, 10, 20, "2022-06-20", 15, "2022-04-10", '2022-04-10 10:00', 18, 2, 2);
 
 
